7d73fe60271f3721228cb05b3dc827d1cbb9be75
nagamocha3000 <nagamocha@gmail.com>
2019-10-09 13:44:54 +0300

    analyze bundle

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,5 +56,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-webpack-bundle-analyzer",
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: "static",
+      },
+    },
   ],
 }
